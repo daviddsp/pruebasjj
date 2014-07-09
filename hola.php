a32f9e4f3fdb1a26ba5e192957e12d46e91db8b4
David Solórzano <abdsp22@gmail.com>
2014-07-08 23:31:13 -0430

    Agregue un ejemplo de suma,resta y multiplicación

---
+++ hola.php
@@ -0,0 +1,20 @@
+<?php
+
+echo 'hola';
+$a = 1;
+$b = 2;
+$c = 3;
+
+
+$resultadoSuma = $a + $b;
+$resultadoResta = $a - $c;
+$resultadoMulti = $b * $c;
+
+echo "El resultado de suma es: $resultadoSuma";
+echo "El resultado de resta es: $resultadoResta";
+echo "El resultado de Multiplicación es: $resultadoMulti";
+?>
+
+
+
+
